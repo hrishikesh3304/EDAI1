b4b60b2bbcb7501d566849bd1af67ded83f6492f
ADMIN <codingwithpopatrao@gmail.com>
2023-05-02 11:47:07 +0530

    Hrishikesh new comment - user type

---
+++ app/src/main/java/com/example/firebase3/userType.java
@@ -0,0 +1,56 @@
+package com.example.firebase3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+public class userType extends AppCompatActivity {
+
+    CardView Doctor_Card, Patient_Card;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+
+        Doctor_Card = findViewById(R.id.cardViewStudent);
+        Patient_Card = findViewById(R.id.cardViewTeacher);
+
+        Doctor_Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+                SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                myEdit.putString("User_Type", "Doctor");
+                myEdit.commit();
+                Intent intent = new Intent(userType.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Patient_Card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+                SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                myEdit.putString("User_Type", "Patient");
+                myEdit.commit();
+                Intent intent = new Intent(userType.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+}
